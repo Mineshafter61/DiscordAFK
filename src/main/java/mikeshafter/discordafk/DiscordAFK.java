a2b9a3a147b756834fe8e169212b15729362ebfc
Mineshafter61 <yuhang.061@gmail.com>
2021-05-27 17:28:00 +0900

    Initial commit

---
+++ src/main/java/mikeshafter/discordafk/DiscordAFK.java
@@ -0,0 +1,37 @@
+package mikeshafter.discordafk;
+
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import net.ess3.api.IUser;
+import net.ess3.api.events.AfkStatusChangeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class DiscordAFK extends JavaPlugin implements Listener {
+  
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+    this.getServer().getConsoleSender().sendMessage(ChatColor.AQUA+"DiscordAFK has been invoked!");
+    Bukkit.getPluginManager().registerEvents(this, this);
+  }
+  
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+    this.getServer().getConsoleSender().sendMessage(ChatColor.RED+"DiscordAFK has been disabled!");
+  }
+  
+  @EventHandler
+  public void onAFK(AfkStatusChangeEvent event) {
+    IUser iUser = event.getAffected();
+    Player player = iUser.getBase();
+    String name = player.getDisplayName();
+    DiscordUtil.sendMessage(DiscordUtil.getTextChannelById(DiscordSRV.config().get("Channels.global")), name+" is now AFK!");
+  }
+}
